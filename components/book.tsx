a0cd1fbf175d8faa0b1e00108dcf2d234e389930
koolshibe <porgsgomeep@gmail.com>
2024-06-08 14:56:19 -0700

    book

--- components/book.tsx
+++ components/book.tsx
@@ -1,3 +1,12 @@
-export default function book() {
-    
+import Image from "next/image";
+export default function book(props:any) {
+    return (
+        <div>
+            <img src={props.args[0]}></img>
+            <div>
+                <h6>{props.args[1]}</h6>
+                <p>{props.args[2]}</p>
+            </div>
+        </div>
+    );
 }
