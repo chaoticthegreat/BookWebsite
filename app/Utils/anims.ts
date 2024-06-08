767f50c9aff4e841b7e3e8a2e693a5a6ca9f4f1a
koolshibe <porgsgomeep@gmail.com>
2024-06-08 13:51:17 -0700

    Home

---
+++ app/Utils/anims.ts
@@ -0,0 +1,9 @@
+import gsap from "gsap";
+
+// get other plugins:
+import ScrollTrigger from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger); 
+
+const hoverGradient = () => {
+    const tl =  gsap.timeline({});
+}
